ea54361ab0dd1253e8bbdb6dbc26cdcf2e4c7a18
ryanzinho653 <81655866+ryanzinho653@users.noreply.github.com>
2021-05-23 15:02:33 -0300

    teste025

---
+++ exercicios_C#/aula001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aula001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("primeiro programa em C#");
+
+            Console.ReadLine();
+        }
+    }
+}
