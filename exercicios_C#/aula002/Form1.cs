572307fc91ce24c4fba754be0dc4a7aa3c28b2e1
ryanzinho653 <81655866+ryanzinho653@users.noreply.github.com>
2021-06-12 21:20:37 -0300

    teste036

---
+++ exercicios_C#/aula002/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace aula002
+{
+
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int sum = 0;
+
+            for (int number = 2; number <= 100; number += 2)
+                sum += number;
+            
+            MessageBox.Show("a soma e: " + sum, "soma pares inteiros de 2 a 100", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
